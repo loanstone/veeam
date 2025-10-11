8c5002ebcab897acef0db38da05986e38e339a00
loanstone <33924427+loanstone@users.noreply.github.com>
2025-10-11 12:37:11 +0200

    fixed syncPeriod + log file arguments added md5 added get files and folders

--- FolderSync/Program.cs
+++ FolderSync/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
 
 namespace FolderSync
 {
@@ -17,6 +18,7 @@ namespace FolderSync
             platform = Environment.OSVersion.Platform.ToString();
             IConfiguration config = new ConfigurationBuilder().AddCommandLine(args).Build();
             ParseArgs(config);
+            Sync();
         }
 
         private static void ParseArgs(IConfiguration config)
@@ -26,31 +28,33 @@ namespace FolderSync
                 switch (kvp.Key)
                 {
                     case "syncPeriod":
-                        if (!Int32.TryParse(kvp.Value, out syncPeriod))
+                        if (!Int32.TryParse(kvp.Value, out int tempPeriod) && tempPeriod <= 0)
                         {
-                            Console.WriteLine($"{kvp.Value} is not a valid integer for sync. Reverting back to default 60 minutes.");
+                            Console.WriteLine($"{kvp.Value} is not a valid integer for sync. Defaulting to 60 minutes.");
                             syncPeriod = 60;
                         }
+                        else
+                            syncPeriod = tempPeriod;
                         break;
                     case "sourceFolder":
-                        if (DoesDirExist(kvp.Value))
+                        if (Directory.Exists(kvp.Value))
                             sourceFolder = kvp.Value;
                         else
                             InvalidPath(kvp.Value);                    
                         break;
                     case "destFolder":
-                        if (DoesDirExist(kvp.Value))
+                        if (Directory.Exists(kvp.Value))
                             destinationFolder = kvp.Value;
                         else
                             InvalidPath(kvp.Value);
                         break;
                     case "log":
-                        if (DoesDirExist(Path.GetDirectoryName(kvp.Value)))
-                            logFilePath = kvp.Value;
+                        if (Directory.Exists(kvp.Value))
+                            logFilePath = Path.Combine(kvp.Value, $"SyncLog_{DateTime.Now.ToString("dd-MM-yyyy HH:mm")}.log");
                         else
                         {
                             Console.WriteLine($"The directory {kvp.Value} does not exist, defaulting to {Path.Combine(Directory.GetCurrentDirectory(), "Log.log")}");
-                            logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Log.log");
+                            logFilePath = Path.Combine(Directory.GetCurrentDirectory(), $"SyncLog_{DateTime.Now.ToString("dd-MM-yyyy HH:mm")}.log");
                         }
                         break;
                     default:
@@ -59,16 +63,72 @@ namespace FolderSync
                 }
             }
         }
-
-        private static bool DoesDirExist(string argPath)
-        {
-            return Directory.Exists(argPath);
-        }
-        
         private static void InvalidPath(string value)
         {
             Console.WriteLine($"{value} is not a valid directory on your system");
             Environment.Exit(0);
+        }
+
+        private static void Sync()
+        {
+            string[] sourceFiles = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories);
+            string[] sourceDirs = Directory.GetDirectories(sourceFolder, "*", SearchOption.AllDirectories);
+            string[] destFiles = Directory.GetFiles(destinationFolder, "*", SearchOption.AllDirectories);
+            string[] destDirs = Directory.GetDirectories(destinationFolder, "*", SearchOption.AllDirectories);
+
+
+        }
+        
+        // private static bool AreEqual(string[] source, string[] destination)
+        // {
+        //     if (source.Length != destination.Length)
+        //         return false;
+            
+        // }
+
+        private static string CalculateMD5(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLower();
+                }
+            }
+        }
+
+        private static bool CompareMd5(string checksum1, string checksum2)
+        {
+            return String.Equals(checksum1, checksum2, StringComparison.OrdinalIgnoreCase);
+        }
+
+        enum Actions
+        {
+            Create,
+            Delete,
+            Copy,
+            Rename,
+            Move
+        }
+
+        private static void Log(string name, Actions action)
+        {
+            switch (action)
+            {
+                case Actions.Create:
+                    break;
+                case Actions.Delete:
+                    break;
+                case Actions.Copy:
+                    break;
+                case Actions.Rename:
+                    break;
+                case Actions.Move:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Launch with the following arguments:
 --syncPeriod "10"
 --source "/home/sampleSource"
 --dest "/home/sampleDest"
---log "/home/Log.log"
+--log "/home/Log" &rarr; log is saved as *SyncLog_day-month-year hour:minute.log*
